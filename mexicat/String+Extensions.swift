3aa177d6953a8f15164f1762d806af27465f6003
Gmo Ginppian <davidguillermo.lopez.contractor@bbva.com>
2018-07-30 11:54:01 -0500

    initial commit

---
+++ mexicat/String+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensiones.swift
+//  mexicat
+//
+//  Created by Gmo Ginppian on 30/07/18.
+//  Copyright © 2018 BUAP. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static func mexi() -> String
+    {
+        return "callate mexicat, no digas perrradas..."
+    }
+}
---
+++ mexicat/mexicat.h
@@ -0,0 +1,19 @@
+//
+//  mexicat.h
+//  mexicat
+//
+//  Created by Gmo Ginppian on 30/07/18.
+//  Copyright © 2018 BUAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mexicat.
+FOUNDATION_EXPORT double mexicatVersionNumber;
+
+//! Project version string for mexicat.
+FOUNDATION_EXPORT const unsigned char mexicatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mexicat/PublicHeader.h>
+
+
